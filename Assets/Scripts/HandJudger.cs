57f57b7b70f523c545047d67af3acb5663dd8ed5
epen3420 <epen3420@gmail.com>
2025-11-30 23:55:27 +0900

    Update HandJudger.cs

--- Assets/Scripts/HandJudger.cs
+++ Assets/Scripts/HandJudger.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public enum ResultType
 {
     None = -1,
@@ -5,8 +8,39 @@ public enum ResultType
     Win,
     Lose,
 }
+
+public struct HandResultTypePair
+{
+    public ResultType result;
+    public HandType winHandType;
+}
+
 public class HandJudger
 {
+    public HandResultTypePair Judge(IEnumerable<Hand> hands)
+    {
+        // 重複を削除
+        var uniqueHands = hands.Distinct();
+        int uniqueHandCount = uniqueHands.Count();
+
+        // 一種類か三種類全部か
+        if (uniqueHandCount == 1 ||
+           uniqueHandCount == 3)
+        {
+            var result = new HandResultTypePair
+            {
+                result = ResultType.Draw,
+                winHandType = HandType.None,
+            };
+            return ResultType.Draw;
+        }
+
+        var me = uniqueHands.ElementAt(0);
+        var enemy = uniqueHands.ElementAt(1);
+
+        return Judge(me, enemy);
+    }
+
     public ResultType Judge(Hand me, Hand enemy)
     {
         if (me.HandType == enemy.HandType)
