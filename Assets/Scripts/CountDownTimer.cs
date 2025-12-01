c35e3a55a4f46546a4c8056009b8ed44de22506f
epen3420 <epen3420@gmail.com>
2025-12-01 10:02:02 +0900

    コミットし忘れ

--- Assets/Scripts/CountDownTimer.cs
+++ Assets/Scripts/CountDownTimer.cs
@@ -1,113 +1,109 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
-using MiniGame.Event;
 using Time = UnityEngine.Time;
 using Debug = UnityEngine.Debug;
 
-namespace MiniGame.Core
+/// <summary>
+/// CountDownします
+/// </summary>
+public class CountDownTimer
 {
-    /// <summary>
-    /// CountDownします
-    /// </summary>
-    public class CountDownTimer
+    private readonly FloatEventChannelSO OnChangeTime;
+    public readonly VoidEventChannelSO OnTimeZero;
+
+    public float InitTime => initTime;
+    public float CurrentTime => currentTime;
+
+    private float initTime;
+    private float currentTime;
+    private bool isCounting = false;
+    private bool zeroFired = false;
+    private CancellationTokenSource resetCts;
+
+
+    public CountDownTimer(FloatEventChannelSO changeTimeEventChannel,
+                          VoidEventChannelSO timeZeroEventChannel)
     {
-        private readonly FloatEventChannelSO OnChangeTime;
-        public readonly VoidEventChannelSO OnTimeZero;
+        OnChangeTime = changeTimeEventChannel;
+        OnTimeZero = timeZeroEventChannel;
+    }
 
-        public float InitTime => initTime;
-        public float CurrentTime => currentTime;
+    public void Init(float countTime)
+    {
+        StopAndReset();
 
-        private float initTime;
-        private float currentTime;
-        private bool isCounting = false;
-        private bool zeroFired = false;
-        private CancellationTokenSource resetCts;
-
-
-        public CountDownTimer(FloatEventChannelSO changeTimeEventChannel,
-                              VoidEventChannelSO timeZeroEventChannel)
+        if (countTime <= 0)
         {
-            OnChangeTime = changeTimeEventChannel;
-            OnTimeZero = timeZeroEventChannel;
+            Debug.LogWarning($"Can not set {countTime} as timer");
+            return;
         }
 
-        public void Init(float countTime)
+        initTime = countTime;
+        ChangeTime(countTime);
+    }
+
+    public async UniTask Resume(CancellationToken ctn)
+    {
+        if (resetCts == null ||
+            resetCts.IsCancellationRequested)
+        {
+            resetCts = new CancellationTokenSource();
+            CountTime(resetCts.Token).Forget();
+        }
+
+        isCounting = true;
+
+        await UniTask.WaitUntil(() => !isCounting, cancellationToken: ctn);
+    }
+
+    public void StopAndReset()
+    {
+        isCounting = false;
+        zeroFired = false;
+
+        resetCts?.Cancel();
+        resetCts?.Dispose();
+        resetCts = null;
+
+        initTime = 0f;
+        ChangeTime(0f);
+    }
+
+    private void ChangeTime(float time)
+    {
+        currentTime = time;
+        OnChangeTime?.Raise(currentTime);
+
+        if (currentTime <= 0f &&
+            !zeroFired)
+        {
+            zeroFired = true;
+            OnTimeZero?.Raise();
+        }
+    }
+
+    private async UniTaskVoid CountTime(CancellationToken ctn)
+    {
+        try
+        {
+            while (currentTime > 0f)
+            {
+                if (isCounting)
+                {
+                    ChangeTime(currentTime - Time.deltaTime);
+                }
+
+                await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken: ctn);
+            }
+
+            if (currentTime < 0f)
+            {
+                ChangeTime(0f);
+            }
+        }
+        finally
         {
             StopAndReset();
-
-            if (countTime <= 0)
-            {
-                Debug.LogWarning($"Can not set {countTime} as timer");
-                return;
-            }
-
-            initTime = countTime;
-            ChangeTime(countTime);
-        }
-
-        public async UniTask Resume(CancellationToken ctn)
-        {
-            if (resetCts == null ||
-                resetCts.IsCancellationRequested)
-            {
-                resetCts = new CancellationTokenSource();
-                CountTime(resetCts.Token).Forget();
-            }
-
-            isCounting = true;
-
-            await UniTask.WaitUntil(() => !isCounting, cancellationToken: ctn);
-        }
-
-        public void StopAndReset()
-        {
-            isCounting = false;
-            zeroFired = false;
-
-            resetCts?.Cancel();
-            resetCts?.Dispose();
-            resetCts = null;
-
-            initTime = 0f;
-            ChangeTime(0f);
-        }
-
-        private void ChangeTime(float time)
-        {
-            currentTime = time;
-            OnChangeTime?.Raise(currentTime);
-
-            if (currentTime <= 0f &&
-                !zeroFired)
-            {
-                zeroFired = true;
-                OnTimeZero?.Raise();
-            }
-        }
-
-        private async UniTaskVoid CountTime(CancellationToken ctn)
-        {
-            try
-            {
-                while (currentTime > 0f)
-                {
-                    if (isCounting)
-                    {
-                        ChangeTime(currentTime - Time.deltaTime);
-                    }
-
-                    await UniTask.Yield(PlayerLoopTiming.Update, cancellationToken: ctn);
-                }
-
-                if (currentTime < 0f)
-                {
-                    ChangeTime(0f);
-                }
-            }
-            finally
-            {
-                StopAndReset();
-            }
         }
     }
 }
