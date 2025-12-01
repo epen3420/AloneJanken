6cf4b8b3f62f2dc2dd9ac8cf3d605f35373655bb
epen3420 <epen3420@gmail.com>
2025-12-01 21:33:39 +0900

    feat: UIの更新

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text questText;
+    [SerializeField]
+    private TMP_Text targetHandText; // 仮置きいずれアウトラインみたいな実装になる
+    [Header("Events")]
+    [SerializeField]
+    private QuestEventChannelSO startRound;
+    [SerializeField]
+    private BoolEventChannelSO endRound;
+    [SerializeField]
+    private FloatEventChannelSO changeTimeEvent;
+
+
+    private void OnEnable()
+    {
+        startRound.OnRaised += ShowQuest;
+        startRound.OnRaised += ShowTargetHand;
+    }
+
+    private void OnDisable()
+    {
+        startRound.OnRaised -= ShowQuest;
+        startRound.OnRaised -= ShowTargetHand;
+    }
+
+    private void ShowQuest(JankenQuestBase quest)
+    {
+        questText.SetText(quest.name);
+    }
+
+    private void ShowTargetHand(JankenQuestBase quest)
+    {
+        string targetStr = string.Join("", quest.HandPair.Select(pair => $"{pair.OwnerPos}, {pair.HandType} \n"));
+
+        targetHandText.SetText($"{targetStr}");
+    }
+}
