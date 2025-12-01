059be9dffdd9016335ec4c4788e8c67ed6997566
epen3420 <epen3420@gmail.com>
2025-12-01 20:38:28 +0900

    refactor: ラウンドのリファクタ

--- Assets/Scripts/RoundManager.cs
+++ Assets/Scripts/RoundManager.cs
@@ -11,14 +11,18 @@ public class RoundManager : MonoBehaviour
     private int beatsNum = 7;
     [SerializeField]
     private JankenInputManager inputs;
+
+    [Header("Events")]
+    [SerializeField]
+    private VoidEventChannelSO startRound;
     [SerializeField]
     private FloatEventChannelSO changeTimeEvent;
+
     [SerializeField]
     private QuestDatabase questDb;
 
     private CountDownTimer timer;
     private bool isPlaying = false;
-
 
     private void Awake()
     {
@@ -30,29 +34,50 @@ public class RoundManager : MonoBehaviour
         if (isPlaying) return;
         isPlaying = true;
 
-        int roundCount = 0;
-        while (questDb.Quests.Count() > roundCount)
+        int totalRounds = questDb.Quests.Length;
+
+        try
         {
-            inputs.Enable();
-            timer.Init(jankenBpm / 60 * beatsNum);
-            await timer.Resume(ctn);
-            inputs.Disable();
-
-            var inputResult = inputs.GetCurrentInputHand();
-            bool isWin = false;
-            if (inputResult.Count() > 0)
+            for (int roundCount = 0; roundCount < totalRounds; roundCount++)
             {
-                var resultHands = HandJudger.Judge(inputResult);
-                isWin = questDb.Quests[roundCount].Judge(resultHands);
+                // キャンセルされているかチェック
+                ctn.ThrowIfCancellationRequested();
 
-                foreach (var hand in resultHands)
+                startRound.Raise();
+
+                inputs.Enable();
+                try
                 {
-                    Debug.Log(hand.ToString());
-                }
-            }
+                    float duration = 60f / jankenBpm * beatsNum;
+                    timer.Init(duration);
 
-            Debug.Log(isWin);
-            roundCount++;
+                    await timer.Resume(ctn);
+                }
+                finally
+                {
+                    inputs.Disable();
+                }
+
+                var inputResult = inputs.GetCurrentInputHand();
+
+                bool isWin = false;
+                if (inputResult.Any())
+                {
+                    var resultHands = HandJudger.Judge(inputResult);
+                    isWin = questDb.Quests[roundCount].Judge(resultHands);
+
+                    foreach (var hand in resultHands)
+                    {
+                        Debug.Log($"Hand: {hand}");
+                    }
+                }
+
+                Debug.Log($"Round {roundCount + 1}: Win = {isWin}");
+            }
+        }
+        finally
+        {
+            isPlaying = false;
         }
     }
 }
