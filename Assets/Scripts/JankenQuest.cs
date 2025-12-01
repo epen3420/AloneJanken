ad568a6be6991058ce02f32511f6823ef18798a2
epen3420 <epen3420@gmail.com>
2025-12-01 14:52:27 +0900

    feat: じゃんけんのお題の抽象クラスの実装

---
+++ Assets/Scripts/JankenQuest.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class JankenQuestBase
+{
+    public abstract string Name { get; }
+    public abstract string DisplayDescription { get; }
+
+    protected abstract bool InternalJudge(List<HandResultTypePair> handResultPairs);
+
+    public bool Judge(IEnumerable<HandResultTypePair> handResultPairs)
+    {
+        var pairs = handResultPairs.ToList();
+        if (pairs.Count < 2)
+        {
+            throw new System.Exception("二人以下のじゃんけんは判定できません");
+        }
+
+        var uniqueHandPosCount = pairs.Select(pair => pair.Hand.OwnerPos)
+                                      .Distinct()
+                                      .Count();
+
+        // 同じ手を使っていたらミス
+        if (pairs.Count > uniqueHandPosCount)
+        {
+            return false;
+        }
+
+        return InternalJudge(pairs);
+    }
+}
