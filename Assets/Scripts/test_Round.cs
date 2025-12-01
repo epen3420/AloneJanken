78af0d676b532ee1add3afaaeafdbf46f0523c45
epen3420 <epen3420@gmail.com>
2025-12-01 16:23:21 +0900

    test: あいこのみ判定するラウンドのテスト

--- Assets/Scripts/test_Round.cs
+++ Assets/Scripts/test_Round.cs
@@ -5,9 +5,11 @@ public class test_Round : MonoBehaviour
 {
     [SerializeField]
     private JankenInputManager inputManager;
+    [SerializeField]
+    private QuestDatabase questDb;
 
     public void StartRound()
     {
-        new RoundManager(new CountDownTimer(null, null), inputManager).Execute(destroyCancellationToken).Forget();
+        new RoundManager(new CountDownTimer(null, null), inputManager).Execute(questDb.Quests[0], destroyCancellationToken).Forget();
     }
 }
