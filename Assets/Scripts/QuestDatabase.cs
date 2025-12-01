076a450f33d25afbb3d2768eaa06e453d6aa5817
epen3420 <epen3420@gmail.com>
2025-12-01 16:21:48 +0900

    feat: シリアル化とMenuで作成可能に

--- Assets/Scripts/QuestDatabase.cs
+++ Assets/Scripts/QuestDatabase.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "NewQuestDB", menuName = "Quests/Database")]
 public class QuestDatabase : ScriptableObject
 {
+    [SerializeField]
     private JankenQuestBase[] quests;
 
     public JankenQuestBase[] Quests => quests;
