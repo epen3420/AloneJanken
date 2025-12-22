533f793159ed278b818520ad55281c2f1bb13401
epen3420 <epen3420@gmail.com>
2025-12-22 14:22:11 +0900

    リザルトの表示

--- Assets/Scripts/UI/ResultHekatonView.cs
+++ Assets/Scripts/UI/ResultHekatonView.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,15 +11,48 @@ public class ResultHekatonView : MonoBehaviour
         public string SceneName;
         public Sprite sprite;
     }
+    [System.Serializable]
+    private struct SceneHekatonMapByScore
+    {
+        [System.Serializable]
+        public struct SpriteScoreMap
+        {
+            public int score;
+            public Sprite sprite;
+        }
+
+        public string SceneName;
+        public SpriteScoreMap[] scores;
+    }
 
     [SerializeField]
     private SceneHekatonMap[] sceneHekatonMaps;
     [SerializeField]
+    private SceneHekatonMapByScore[] hekatonMapByScores;
+    [SerializeField]
     private Image image;
 
 
-    private void Awake()
+    private void Start()
     {
-        image.sprite = sceneHekatonMaps.FirstOrDefault(map => map.SceneName == SceneController.PreviousSceneName).sprite;
+        List<SceneHekatonMapByScore.SpriteScoreMap> spriteScoreMaps =
+            hekatonMapByScores.FirstOrDefault(map => map.SceneName == SceneController.PreviousSceneName)
+            .scores
+            .ToList();
+
+        spriteScoreMaps.Sort((a, b) =>
+        {
+            if (a.score < b.score) return 1;
+            else return -1;
+        });
+
+        var score = ScoreManager.Instance.GetCurrentScore();
+        foreach (var spriteScoreMap in spriteScoreMaps)
+        {
+            if (score < spriteScoreMap.score)
+            {
+                image.sprite = spriteScoreMap.sprite;
+            }
+        }
     }
 }
