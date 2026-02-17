a8667671fc56f9b9416611a23c8abbde5b3190e4
epen3420 <epen3420@gmail.com>
2026-02-17 15:02:04 +0900

    feat: 上の腕の出現アニメーション

--- Assets/Scripts/UI/HekatonAnimator.cs
+++ Assets/Scripts/UI/HekatonAnimator.cs
@@ -1,51 +1,25 @@
-using System.Linq;
 using UnityEngine;
 
 public class HekatonAnimator : MonoBehaviour
 {
     [SerializeField]
-    private Animator[] upHandAnimators;
-    [SerializeField]
-    private Animator[] downHandAnimators;
+    private Animator animator;
     [SerializeField]
     private VoidEventChannelSO startNormalLevel;
-    [SerializeField]
-    private IntEventChannelSO changeBeats;
-
-    private Animator[] allAnimators => upHandAnimators.Concat(downHandAnimators).ToArray();
 
 
     private void OnEnable()
     {
         startNormalLevel.OnVoidRaised += AppearHands;
-        changeBeats.OnRaised += JankenAnim;
     }
 
     private void OnDisable()
     {
         startNormalLevel.OnVoidRaised -= AppearHands;
-        changeBeats.OnRaised -= JankenAnim;
     }
 
     private void AppearHands()
     {
-        foreach (var animator in upHandAnimators)
-        {
-            animator.SetTrigger("Appear");
-        }
-    }
-
-    private void JankenAnim(int count)
-    {
-        if (count != 0) return;
-
-        foreach (var animator in allAnimators)
-        {
-            // float speed = animator.GetNextAnimatorClipInfo(0)
-            //      .FirstOrDefault(state => state.clip.name == "Janken")
-            //      .clip.length / 8;
-            // animator.SetFloat("MotionTime", speed);
-            animator.SetTrigger("Janken");
-        }
+        animator.SetTrigger("Appear");
     }
 }
